2dbbc0b87ea6105023c9df42056a9f388556a4fe
Juansubas <juansubas@gmail.com>
2022-11-24 21:24:52 -0500

    Primer commit

--- proyecto_back/config/database.js
+++ proyecto_back/config/database.js
@@ -5,7 +5,10 @@ const port = "27017";
 const db = "hr";
 
 exports.mongoConnect = () => {
-  const mongoStringConnection = `mongodb://${host}:${port}/${db}`;
+  //Para local
+  //const mongoStringConnection = `mongodb://${host}:${port}/${db}`;
+  //Para internet (quitamos test)
+  const mongoStringConnection = `mongodb+srv://admin:admin@videogame.ae2hpnn.mongodb.net/goza-finanzas`;
   mongoose.connect(mongoStringConnection);
   mongoose.Promise = global.Promise;
   const dbConnection = mongoose.connection;
