63fa64e3fb7a189e33333aaeade07f557cd8d1e0
Juansubas <juansubas@gmail.com>
2022-11-24 23:16:09 -0500

    Actualizando Hipervinculo

--- proyecto_front/src/components/index/index.js
+++ proyecto_front/src/components/index/index.js
@@ -9,7 +9,9 @@ export default class inicio extends React.Component {
       <><h1 style={{ marginTop: 300 }}>
         <br />
         GozaFinanzas - Welcome
-      </h1><h2>Es un sitio web desarrollado con el proposito de ayudarte a gestionar tus finanzas personales una forma simple pero efectiva, buscando el mayor bienestar posible a la economía del ciudadano común.</h2></>
+      </h1><h2>Es un sitio web desarrollado con el proposito de ayudarte a gestionar tus finanzas personales una forma simple pero efectiva, buscando el mayor bienestar posible a la economía del ciudadano común.</h2>
+      <a href="../login">Inicia Sesión</a>
+      </>
     );
   }
 }
