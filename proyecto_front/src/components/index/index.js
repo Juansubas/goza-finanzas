4913f7b387fa37a2c20edb173ffac4127e2f6e60
Juansubas <juansubas@gmail.com>
2022-11-24 23:25:22 -0500

    Organizando Login

--- proyecto_front/src/components/index/index.js
+++ proyecto_front/src/components/index/index.js
@@ -6,7 +6,7 @@ export default class inicio extends React.Component {
   }
   render() {
     return (
-      <><h1 style={{ marginTop: 200 }}>
+      <><h1 style={{ marginTop: 400 }}>
         <br />
         GozaFinanzas - Welcome
       </h1><h2>Es un sitio web desarrollado con el proposito de ayudarte a gestionar tus finanzas personales una forma simple pero efectiva, buscando el mayor bienestar posible a la economía del ciudadano común.</h2>
